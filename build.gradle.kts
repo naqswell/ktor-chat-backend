88de07bcc17f2c24a3954df69aeb7c2111581d67
naqswell <naqswell@gmail.com>
2024-06-10 22:38:23 +0300

    build: Migrate Gradle dependencies to Version Catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,28 @@
-
+val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("io.ktor.plugin") version "2.3.11"
-    id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.conventionalCommits)
 }
 
 group = "com.naqswell"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.naqswell.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+/*  accepted types: feat, fix, build, chore, ci, docs, style, refactor, perf, test, tmp
+    simplified commit format: 'type(scope): message'
+    example commit: 'feat(android): Added app widget'
+    see https://www.conventionalcommits.org/en/v1.0.0/ for more details on conventional commits
+*/
 conventionalCommits {
     warningIfNoGitRoot = true
     successMessage = "Commit message meets Conventional Commit standards..."
@@ -29,9 +34,8 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.kotlin.test.junit)
 }
