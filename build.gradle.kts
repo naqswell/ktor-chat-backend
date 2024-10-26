b0470a3154cd473c2b6b36a40f7c5673ac9ac143
Nikita <ivanov.nikita@dialpro-llc.com>
2024-10-26 12:27:48 +0300

    refactor: Change dependencies from direct calls in build.gradle.kts to aliases from toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.41.0"
-    id("nl.littlerobots.version-catalog-update") version "0.8.5"
+    alias(libs.plugins.benManesVersions)
+    alias(libs.plugins.nlittlerobotsVersionCatalogUpdate)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.conventionalCommits)
@@ -40,7 +40,6 @@ dependencies {
     implementation(libs.bundles.ktor.server)
     implementation(libs.logback.classic)
     testImplementation(libs.ktor.server.tests)
-//    testImplementation(libs.kotlin.test.junit)
 
     implementation(libs.bundles.hoplite)
 
